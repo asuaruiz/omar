7650c48ed579fdb6cd62b9439120cf33cb9e3b72
andrea <asuaruiz@gmail.com>
2017-07-08 00:08:57 -0400

    improve

---
+++ js/main.js
@@ -0,0 +1,54 @@
+console.log('So are the script files!!!');
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 200) {
+    $(".s1").fadeIn();
+  } else {
+    $(".s1").fadeOut();
+  }
+  if ($(document).scrollTop() > 950) {
+    $(".s2").fadeIn();
+  } else {
+    $(".s2").fadeOut();
+  }
+  if ($(document).scrollTop() > 1500) {
+    $(".s3").fadeIn();
+  } else {
+    $(".s3").fadeOut();
+  }
+  if ($(document).scrollTop() > 2300) {
+    $(".s4").fadeIn();
+  } else {
+    $(".s4").fadeOut();
+  }
+  if ($(document).scrollTop() > 3300) {
+    $(".s5").fadeIn();
+  } else {
+    $(".s5").fadeOut();
+  }
+  if ($(document).scrollTop() > 4000) {
+    $(".s6").fadeIn();
+  } else {
+    $(".s6").fadeOut();
+  }
+  if ($(document).scrollTop() > 5200) {
+    $(".s7").fadeIn();
+  } else {
+    $(".s7").fadeOut();
+  }
+  if ($(document).scrollTop() > 6500) {
+    $(".s8").fadeIn();
+  } else {
+    $(".s8").fadeOut();
+  }
+  if ($(document).scrollTop() > 7800) {
+    $(".s9").fadeIn();
+  } else {
+    $(".s9").fadeOut();
+  }
+  if ($(document).scrollTop() > 8200) {
+    $(".s10").fadeIn();
+  } else {
+    $(".s10").fadeOut();
+  }
+});
