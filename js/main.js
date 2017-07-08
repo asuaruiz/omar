51e26efb8de780c0a981973aff7d32387fb05a63
Andrea Suarez <asuaruiz@gmail.com>
2017-07-08 00:32:55 -0400

    improve js

--- js/main.js
+++ js/main.js
@@ -6,47 +6,47 @@ $(window).scroll(function() {
   } else {
     $(".s1").fadeOut();
   }
-  if ($(document).scrollTop() > 700) {
+  if ($(document).scrollTop() > 600) {
     $(".s2").fadeIn();
   } else {
     $(".s2").fadeOut();
   }
-  if ($(document).scrollTop() > 1200) {
+  if ($(document).scrollTop() > 1100) {
     $(".s3").fadeIn();
   } else {
     $(".s3").fadeOut();
   }
-  if ($(document).scrollTop() > 1800) {
+  if ($(document).scrollTop() > 1600) {
     $(".s4").fadeIn();
   } else {
     $(".s4").fadeOut();
   }
-  if ($(document).scrollTop() > 2500) {
+  if ($(document).scrollTop() > 2100) {
     $(".s5").fadeIn();
   } else {
     $(".s5").fadeOut();
   }
-  if ($(document).scrollTop() > 3100) {
+  if ($(document).scrollTop() > 2600) {
     $(".s6").fadeIn();
   } else {
     $(".s6").fadeOut();
   }
-  if ($(document).scrollTop() > 3700) {
+  if ($(document).scrollTop() > 3100) {
     $(".s7").fadeIn();
   } else {
     $(".s7").fadeOut();
   }
-  if ($(document).scrollTop() > 4500) {
+  if ($(document).scrollTop() > 3600) {
     $(".s8").fadeIn();
   } else {
     $(".s8").fadeOut();
   }
-  if ($(document).scrollTop() > 5200) {
+  if ($(document).scrollTop() > 5100) {
     $(".s9").fadeIn();
   } else {
     $(".s9").fadeOut();
   }
-  if ($(document).scrollTop() > 5900) {
+  if ($(document).scrollTop() > 5600) {
     $(".s10").fadeIn();
   } else {
     $(".s10").fadeOut();
